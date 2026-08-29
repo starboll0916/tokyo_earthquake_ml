3401757fafce89938099bf7c4d6e028b9eef8b55
Lee Jinwoo <starboll09@gmail.com>
2026-08-29 22:34:24 +0900

    수정만 조금함

---
+++ phase1_real.py
@@ -0,0 +1,78 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+
+
+
+df = pd.read_csv(r"C:\Users\sglee\Downloads\query (3).csv")
+
+
+
+analysis_df = df[
+ ["time",
+  "latitude",
+  "longitude",
+  "depth",
+  "mag",
+  "place"]
+  ].copy()
+
+"""print(analysis_df.head())"""
+
+analysis_df["time"] = pd.to_datetime(
+    analysis_df["time"]
+)
+
+analysis_df["year"] = analysis_df["time"].dt.year
+analysis_df["month"] = analysis_df["time"].dt.month
+
+def classify_region(lat, lon):
+
+    # Tokyo / Kanto
+    if 34.5 <= lat <= 37.5 and 138 <= lon <= 142:
+        return "Kanto"
+
+    # Nagoya / Chubu
+    elif 34 <= lat <= 37 and 136 <= lon < 138:
+        return "Chubu"
+
+    # Osaka / Kansai
+    elif 33 <= lat <= 36 and 134 <= lon < 136:
+        return "Kansai"
+
+    else:
+        return "Other"
+
+analysis_df["region"] = analysis_df.apply(
+    lambda row: classify_region(
+        row["latitude"],
+        row["longitude"]
+    ),
+    axis=1
+)
+
+
+"""mean magnititude 4.459
+depth 78.36"""
+
+region_summary = analysis_df.groupby("region").agg(
+    earthquake_count=("mag", "count"),
+    mean_magnitude=("mag", "mean"),
+    max_magnitude=("mag", "max"),
+    mean_depth=("depth", "mean"),
+    max_depth=("depth", "max")
+)
+
+print(region_summary)
+print(analysis_df)
+
+
+region_summary.to_csv(
+    "data/processed/earthquakes_cleaned.csv",
+    index=False
+)
+
+
+
+print(os.path.abspath("data/processed/earthquakes_cleaned.csv"))
